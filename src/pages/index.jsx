55d47bf26d8883930cd4e27af51173fa66d23a2f
Lenka Kotková <lenka.nejedla@gmail.com>
2025-06-23 21:57:12 +0200

    prvni pokus

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { render } from "@czechitas/render";
 import "../global.css";
 import "./index.css";
+import { Episode } from "../components/Episode";
 
 const podcast1 = {
   num: "126",
@@ -8,10 +9,40 @@ const podcast1 = {
   guest: "Radovan Siwek",
 };
 
+const episodes = [
+  { num: 126, title: 'Robot, který snědl koblihu', guest: 'Radovan Siwek' },
+  { num: 127, title: 'Hledání plyšového Yettiho', guest: 'Vojtěch Ryba' },
+  { num: 128, title: 'Moderní hrachová polévka', guest: 'Kamila Štancová' },
+  { num: 129, title: 'Poloautomatické zrcadlo', guest: 'Janka Janovská' },
+  { num: 130, title: 'Máčené hlavy parlamentu', guest: 'Jonáš Daněk' },
+  { num: 131, title: 'Služby na hraně přívěsu', guest: 'Tereza Křivánková' },
+  { num: 132, title: 'Klasifikace sněžných klokanů', guest: 'Josef Stix' },
+  { num: 133, title: 'Rybolov v Oceánu bouří', guest: 'Ludmila Gajová' },
+];
+
 document.querySelector("#root").innerHTML = render(
   <>
     <div className="episodes-list">
-      <div className="episode">
+      <h2>Výpis pomocí komponenty</h2>
+      <Episode
+        num={podcast1.num}
+        title={podcast1.title}
+        guest={podcast1.guest}
+      />
+
+      <h2>Výpis pomocí map</h2>
+     
+      {episodes.map((episod) => ( 
+        <Episode
+          key={episod.num} 
+          num={episod.num}
+          title={episod.title}
+          guest={episod.guest}
+        />           
+      ))}
+
+    {/* Puvodni kod */}
+      {/* <div className="episode">
         <div className="episode__num">127</div>
         <div className="episode__body">
           <div className="episode__title">Hledání plyšového Yettiho</div>
@@ -25,7 +56,7 @@ document.querySelector("#root").innerHTML = render(
           <div className="episode__title">Moderní hrachová kaše</div>
           <div className="episode__guest">host: Kamila Štancová</div>
         </div>
-      </div>
+      </div> */}
     </div>
   </>
 );
